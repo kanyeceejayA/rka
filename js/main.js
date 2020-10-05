9e4dbefb27fc03c9a6e40931d8a1744a0b97d1f3
kanyeceejayA <33685273+kanyeceejayA@users.noreply.github.com>
2020-10-05 16:53:43 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,99 @@
+/* =================================
+------------------------------------
+	Cryptocurrency - Landing Page Template
+	Version: 1.0
+ ------------------------------------ 
+ ====================================*/
+
+
+'use strict';
+
+
+$(window).on('load', function() {
+	/*------------------
+		Preloder
+	--------------------*/
+	// $(".loader").fadeOut(); 
+	// $("#preloder").fadeOut("fast");
+
+});
+
+(function($) {
+
+	/*------------------
+		Navigation
+	--------------------*/
+	$('.responsive-bar').on('click', function(event) {
+		$('.main-menu').slideToggle(400);
+		event.preventDefault();
+	});
+
+
+	/*------------------
+		Background set
+	--------------------*/
+	$('.set-bg').each(function() {
+		var bg = $(this).data('setbg');
+		$(this).css('background-image', 'url(' + bg + ')');
+	});
+
+
+	 /*------------------
+		Contact Form
+	--------------------*/
+    $(".check-form").focus(function () {
+        $(this).next("span").addClass("active");
+    });
+    $(".check-form").blur(function () {
+        if ($(this).val() === "") {
+            $(this).next("span").removeClass("active");
+        }
+    });
+
+
+})(jQuery);
+
+// Scrol to top button
+
+let mybutton = document.getElementById("TopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
+	mybutton.style.display = "block";
+  } else {
+	mybutton.style.display = "none";
+  }
+}
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  $('body, html').animate({scrollTop: 0}, 'EaseinOutQuad');
+}
+
+ //smooth scroll
+  // handle links with @href started with '#' only
+  $(document).on('click', 'a[href^="#"]', function(e) {
+    // target element id
+    var id = $(this).attr('href');
+
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+
+    // top position relative to the document
+    var pos = $id.offset().top;
+    
+
+    // animated top scrolling
+    $('body, html').animate({scrollTop: pos},'slow','easeInOutQuad');
+  });
+
